f6611a206fba767440b236aea99807e3ad6d815c
youssefea <youcefea99@gmail.com>
2024-04-22 17:57:54 +0100

    add cache control

--- app/leaderboardApi/route.ts
+++ app/leaderboardApi/route.ts
@@ -63,6 +63,7 @@ export async function GET(req) {
       status: 200,
       headers: {
         "Content-Type": "application/json",
+        "Cache-Control": "private, max-age=30",
       },
     });
   } catch (error) {
